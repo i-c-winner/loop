e6229827a5cd45074689e852007ea82d83da0f3a
Belousov Dmitriy <d-belousov@hotmail.com>
2026-01-04 23:07:19 +0500

    feat: добавит логику в список проектов

--- src/entities/listProjects/ui/List.tsx
+++ src/entities/listProjects/ui/List.tsx
@@ -1,15 +1,26 @@
+'use client'
+
+/**
+ * TODO cделать клиентским компонента
+ */
 import {Box, List, ListItem, Button, Typography} from '@mui/material';
 import styles from './listProjects.module.scss'
 import {CONSTANTS} from "@/shared/config/constants/constant";
+import {redirect} from "next/navigation";
+import {ButtonClickAction} from "@mui/base";
 
 const nodes= Object.values(CONSTANTS.SECTIONS)
 export default function BasicSimpleTreeView() {
+  function handlerClick(event: React.MouseEvent<HTMLButtonElement>) {
+    redirect(`section/${event.currentTarget.textContent}`)
+  }
   return (
     <Box className={styles.list} sx={{ color: 'text.primary'}}>
       <List>
         {nodes.map((node) => (
            <ListItem key={node}>
              <Button
+               onClick={handlerClick}
                sx={{
                  '&:hover': {
                    backgroundColor: 'initial'
