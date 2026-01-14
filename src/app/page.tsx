cdd2b06a3cdf7acf8780e008f631b78b04e895fd
Belousov Dmitriy <d-belousov@hotmail.com>
2026-01-15 00:16:54 +0500

    feat: создаст главную страницу

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,46 @@
-import {Typography} from "@mui/material";
+import {Typography, Card, Box} from "@mui/material";
+import style from './page.module.css'
+
 
 function Home() {
   return (
-    <div>
-      <Typography variant={'h2'}>MAIN PAGE</Typography>
-    </div>
+    <Box className={style.root}>
+      <Card
+      sx={{
+        width: '100%',
+        padding: '16px',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '16px',
+      }}
+      >
+        <Typography
+          sx={{
+            textAlign: 'center',
+          }}
+          variant={'h1'}>Система учета технической документации</Typography>
+        <Typography
+          sx={{
+            textAlign: 'center',
+          }}
+          variant={'subtitle2'}>Точность, прозрачность, полный контроль</Typography>
+      </Card>
+
+      <Box sx={{
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-between',
+      }}>
+        <Card sx={{width: '20%', height: '350px', padding: '16px'}}></Card>
+        <Card sx={{width: '20%', height: '350px', padding: '16px'}}></Card>
+        <Card sx={{width: '20%', height: '350px', padding: '16px'}}></Card>
+        <Card sx={{width: '20%', height: '350px', padding: '16px'}}></Card>
+
+
+      </Box>
+
+
+    </Box>
   );
 }
 export default Home;
